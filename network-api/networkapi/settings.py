bd21436e30250af2b5db49ccfcb7d7375453e2e7
Victoria Chan <victoria.chan@torchbox.com>
2023-06-22 10:56:10 +0100

    Install django-pattern-librray

--- network-api/networkapi/settings.py
+++ network-api/networkapi/settings.py
@@ -91,6 +91,7 @@ env = environ.Env(
     XSS_PROTECTION=bool,
     SCOUT_KEY=(str, ""),
     WAGTAILADMIN_BASE_URL=(str, ""),
+    PATTERN_LIBRARY_ENABLED=(bool, False),
 )
 
 # Read in the environment
@@ -261,6 +262,9 @@ INSTALLED_APPS = list(
             "networkapi.wagtailpages",
             "networkapi.mozfest",
             "networkapi.donate",
+            # TODO: only enable on dev
+            "pattern_library",
+            "networkapi.project_styleguide.apps.ProjectStyleguideConfig",
         ],
     )
 )
@@ -364,6 +368,7 @@ TEMPLATES = [
                 "settings_value": "networkapi.utility.templatetags.settings_value",
                 "wagtailcustom_tags": "networkapi.wagtailcustomization.templatetags.wagtailcustom_tags",
             },
+            "builtins": ["pattern_library.loader_tags"],
         },
     },
 ]
@@ -733,3 +738,25 @@ if DEBUG:
         "127.0.0.1",
         "10.0.2.2",
     ]
+
+# Styleguide
+# Pattern library isn’t intended for production usage, and hasn’t received
+# extensive security scrutiny. Don't enable it on production.
+PATTERN_LIBRARY_ENABLED = env("PATTERN_LIBRARY_ENABLED", default="false").lower() == "true"
+PATTERN_LIBRARY = {
+    # Groups of templates for the pattern library navigation. The keys
+    # are the group titles and the values are lists of template name prefixes that will
+    # be searched to populate the groups.
+    "SECTIONS": (
+        ("components", ["patterns/components"]),
+        ("pages", ["patterns/pages"]),
+    ),
+    # Configure which files to detect as templates.
+    "TEMPLATE_SUFFIX": ".html",
+    # Set which template components should be rendered inside of,
+    # so they may use page-level component dependencies like CSS.
+    "PATTERN_BASE_TEMPLATE_NAME": "patterns/base.html",
+    # Any template in BASE_TEMPLATE_NAMES or any template that extends a template in
+    # BASE_TEMPLATE_NAMES is a "page" and will be rendered as-is without being wrapped.
+    "BASE_TEMPLATE_NAMES": ["patterns/base_page.html"],
+}
